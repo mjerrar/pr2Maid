fba484a068eba10bf6b576678d806966047fecca
Jerrar Bukhari <mjerrar28@live.com>
2018-12-03 22:30:41 -0500

    D-1 Fix CMakeLists.txt and Package.xml for build errors

--- src/PR2Maid.cpp
+++ src/PR2Maid.cpp
@@ -1,5 +1,9 @@
 #include <ros/ros.h>
-#include "include/PR2Maid.h"
+#include "PR2Maid/PR2Maid.h"
+#include "PR2Maid/LegoBrick.h"
+#include "PR2Maid/DropBin.h"
+#include "PR2Maid/BrickList.h"
+
 
 int main(int argc, char** argv)
 {
